db9e851ad19a6af1d222ddf2e438a8d46990d9ec
岡野陽光 <hikaru@okanoyoukounoMacBook-Air.local>
2026-03-02 10:41:56 +1300

    Added basic code

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 console.log("Hellow World");
 console.log("Hikaru Okano");
+
+var name = "Hikaru"; // Function-scoped, hoisted - avoid in modern JS
+let age = 21; //Block-scoped, can be reassighed 
+const PI = 3.14159;
+
+let score = 0;
+console.log(score);
+score = 10;
+console.log(score);
+
+const MAX = 100;
+// MAX = 200;
