0633a4870815f7105fb6bac7fafe025285041ea9
岡野陽光 <hikaru@okanoyoukounoMacBook-Air.local>
2026-03-02 11:22:54 +1300

    Added basic code in JP

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 console.log("Hellow World");
 console.log("Hikaru Okano");
 
-var name = "Hikaru"; // Function-scoped, hoisted - avoid in modern JS
-let age = 21; //Block-scoped, can be reassighed 
-const PI = 3.14159;
+var name = "Hikaru"; // Function-scoped, hoisted - avoid in modern JS 古いバージョン
+let age = 21; //Block-scoped, can be reassighed 　何回でも変更することができる
+const PI = 3.14159;// block-scoped, cannot be reassighned　一度だけしか変更できない。
 
 let score = 0;
 console.log(score);
@@ -12,3 +12,5 @@ console.log(score);
 
 const MAX = 100;
 // MAX = 200;
+
+let homeAddress = "Nikau Appartments"; // camelCase 最初の文字は小文字、その後の文字は大文字　スペース無し
